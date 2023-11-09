b2f4ff570845d48a08f54c8c8119683b3be4efec
Fatwaff <fax.mp4@gmail.com>
2023-11-09 11:52:21 +0700

    add pertemuan07

---
+++ Pertemuan07/Praktikum/p7_1_1214038/android/app/src/main/kotlin/com/example/flutter_application_pertemuan7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_pertemuan7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Pertemuan07/Praktikum/p7_1_1214038/lib/picker.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:intl/intl.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
+
+class MyPicker extends StatefulWidget {
+  const MyPicker({super.key});
+
+  @override
+  State<MyPicker> createState() => _MyPickerState();
+}
+
+class _MyPickerState extends State<MyPicker> {
+  DateTime _dueDate = DateTime.now();
+  final currentDate = DateTime.now();
+  Color _currentColor = Colors.orange;
+  String? _dataFile;
+
+  Widget buildDatePicker(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Date'),
+            TextButton(
+              child: const Text('Select'),
+              onPressed: () async {
+                final selectDate = await showDatePicker(
+                  context: context,
+                  initialDate: currentDate,
+                  firstDate: DateTime(1990),
+                  lastDate: DateTime(currentDate.year + 5),
+                );
+                setState(() {
+                  if (selectDate != null) {
+                    _dueDate = selectDate;
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+        Text(DateFormat('dd-MM-yyyy').format(_dueDate)),
+      ],
+    );
+  }
+
+  Widget buildColorPicker(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Color'),
+        const SizedBox(height: 10),
+        Container(
+          height: 100,
+          width: double.infinity,
+          color: _currentColor,
+        ),
+        const SizedBox(height: 10),
+        Center(
+          child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(_currentColor),
+            ),
+            child: const Text('Pick Color'),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Pick Your Color'),
+                      content: BlockPicker(
+                        pickerColor: _currentColor,
+                        onColorChanged: (color) {
+                          setState(() {
+                            _currentColor = color;
+                          });
+                        },
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            return Navigator.of(context).pop();
+                          },
+                          child: const Text('Save'),
+                        )
+                      ],
+                    );
+                  });
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildFilePicker(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Pick Files'),
+        const SizedBox(height: 10),
+        Center(
+          child: ElevatedButton(
+            child: const Text('Pick and Open Files'),
+            onPressed: () {
+              _pickFile();
+            },
+          ),
+        ),
+        if (_dataFile != null) Text('File Name: $_dataFile'),
+      ],
+    );
+  }
+
+  void _pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    final file = result.files.first;
+    _openFile(file);
+
+    setState(() {
+      _dataFile = file.name;
+    });
+  }
+
+  void _openFile(PlatformFile file) {
+    OpenFile.open(file.path);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Interactive Widget'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            buildDatePicker(context),
+            const SizedBox(height: 20),
+            buildColorPicker(context),
+            const SizedBox(height: 20),
+            buildFilePicker(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
